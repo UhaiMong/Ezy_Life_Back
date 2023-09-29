d01d2fb55d9bbf5b60ffff052491f6478be96eea
Rasalahmad <rasalahmadsit@gmail.com>
2023-09-29 23:15:54 +0600

    wip

--- src/app/modules/users/user.validation.js
+++ src/app/modules/users/user.validation.js
@@ -2,9 +2,11 @@ import { z } from "zod";
 
 const registerUserZodSchema = z.object({
   body: z.object({
-    name: z.string({
-      required_error: "Name is required",
-    }),
+    name: z
+      .string({
+        required_error: "Name is required",
+      })
+      .optional(),
     email: z
       .string({
         required_error: "Email is required",
